e27dc983b1e776743d99a6dbcbec5f88facb3961
kyreniabupt <Kyreniaws@163.com>
2025-08-04 13:51:35 +0800

    basic element

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TableExample />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import TableExample from './components/TableExample.vue';
 
 export default {
-  name: 'App',
+
   components: {
-    HelloWorld
+    TableExample
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui'
+Vue.config.productionTip = false
 
 Vue.config.productionTip = false
+Vue.use(ElementUI)
+import 'element-ui/lib/theme-chalk/index.css'
 
 new Vue({
   render: h => h(App),
